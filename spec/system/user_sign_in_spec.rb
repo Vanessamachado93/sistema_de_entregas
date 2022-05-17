c29efb451cba82d011c7ebfd976b7c4d97565cdc
Vanessa Machado <machado.vanessa32@gmail.com>
2022-05-17 15:53:34 -0300

    Testes unitários de logout

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-describe "Usúario se autentica" do
-  it "com sucesso" do
+describe "User authenticates" do
+  it "successfully" do
     User.create!(email: "email@example.com", password: "password")
 
     visit root_path
@@ -15,9 +15,27 @@ describe "Usúario se autentica" do
     expect(page).to have_content "Login efetuado com sucesso"
     within("nav") do
       expect(page).not_to have_link "Entrar"
-      expect(page).to have_link "Sair"
+      expect(page).to have_button "Sair"
       expect(page).to have_content "email@example.com"
     end
-    # Espera ver uma mensagem de sucesso => Usúario autenticado com sucesso
+  end
+  # Espera ver uma mensagem de sucesso => Usúario autenticado com sucesso
+  it "and logout" do
+    User.create!(email: "email@example.com", password: "password")
+
+    visit root_path
+    click_on "Entrar"
+    fill_in "E-mail", with: "email@example.com"
+    fill_in "Senha", with: "password"
+    within("form") do
+      click_on "Entrar"
+    end
+
+    click_on "Sair"
+
+    expect(page).to have_content "Logout efetuado com sucesso"
+    expect(page).to have_link "Entrar"
+    expect(page).not_to have_button "Sair"
+    expect(page).not_to have_content "emial@example.com"
   end
 end
