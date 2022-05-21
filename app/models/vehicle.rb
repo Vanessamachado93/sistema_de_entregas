6a2b81ce3ee26785de959f0f859b2bd514acd248
Vanessa Machado <machado.vanessa32@gmail.com>
2022-05-21 04:20:57 -0300

    Validações do cadastro dos veículos

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,8 @@
 class Vehicle < ApplicationRecord
+  validates :license_plate, :brand, :code_model, :manufact_year, :capacity, presence: true
+  validates :license_plate, uniqueness: true
+  validates :license_plate, length: { is: 7 }
+  validates :capacity, numericality: { only_integer: true }
+
   belongs_to :shipping_company
 end
